a0fa7a42cd5e781989bec78f6d5fde8be7352c04
Felipe Garcia <lippe-sg@hotmail.com>
2025-11-23 10:31:41 -0300

    Atualização

--- src/main/java/br/com/fiap/gs/filter/CorsFilter.java
+++ src/main/java/br/com/fiap/gs/filter/CorsFilter.java
@@ -12,10 +12,17 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext request,
                        ContainerResponseContext response) throws IOException {
-
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        response.getHeaders().add("Access-Control-Allow-Credentials", "false");
-        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        
+        // Permite TODOS os domínios do Vercel e localhost
+        String origin = request.getHeaderString("Origin");
+        if (origin != null) {
+            response.getHeaders().add("Access-Control-Allow-Origin", origin);
+        }
+        
+        response.getHeaders().add("Access-Control-Allow-Headers", 
+            "origin, content-type, accept, authorization, x-requested-with");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods", 
+            "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
     }
 }
